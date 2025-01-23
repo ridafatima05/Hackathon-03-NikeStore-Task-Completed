629dd438173eb1d2149a1ee7a6bc6e0a8b5e621e
Rida Fatima <162724587+ridafatima05@users.noreply.github.com>
2025-01-23 23:52:37 +0500

    done my all task

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+export {};
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*', // Proxy API routes if needed
+      },
+    ];
+  },
+  images: {
+    domains: ['cdn.sanity.io'], // Add the domain for Sanity images
+  },
+  env: {
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY, // Pass the Clerk secret key from .env.local
+  },
 };
-
-export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import './globals.css'
+import ReduxProvider from "./components/reduxprovider";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+   
+      <html lang="en">
+        <body className={`antialiased`}>
+        <ReduxProvider>
+        <Navbar/>
         {children}
-      </body>
-    </html>
+        <Footer/>
+        </ReduxProvider>
+       </body>
+      </html>
+    
   );
+  
 }
